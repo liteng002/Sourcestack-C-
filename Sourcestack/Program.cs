eea87cf33d5d1c92022b167fe13c896297532eb1
965330241@qq.com <965330241@qq.com>
2021-04-23 16:13:44 +0800

    Add project files.

---
+++ Sourcestack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sourcestack
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            byte a = 255;
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
