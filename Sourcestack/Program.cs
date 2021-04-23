4497f38782056bea1a8ba8346894b383a63f7865
965330241@qq.com <965330241@qq.com>
2021-04-23 19:48:32 +0800

    本地==1

--- Sourcestack/Program.cs
+++ Sourcestack/Program.cs
@@ -8,11 +8,8 @@ namespace Sourcestack
         static void Main(string[] args)
         {
             byte b = 255;
-            byte w = 55;
-            byte g = 25;
 
 
-            Console.WriteLine();
             Console.WriteLine();
 
 
