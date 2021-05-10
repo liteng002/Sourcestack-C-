c19a986a3902cd57092851682c4217e2a9f7837f
965330241@qq.com <965330241@qq.com>
2021-05-10 08:46:30 +0800

    添加一个类，有常量和只读

---
+++ Sourcestack/Repoistory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sourcestack
+{
+    class Repoistory   //能实例就不要静态
+    {
+        const int _version = 0; //1.一个int类型的常量version
+        public static readonly string connection; //2.一个静态只读的字符串connection，以后可用于连接数据库
+    }
+
+}
