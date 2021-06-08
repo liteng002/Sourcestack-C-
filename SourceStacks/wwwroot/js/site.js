0b25829044651d9c6a7632ef16ab9c7aad5de03b
965330241@qq.com <965330241@qq.com>
2021-06-08 14:50:53 +0800

    名称空间作业

--- SourceStacks/wwwroot/js/site.js
+++ SourceStacks/wwwroot/js/site.js
@@ -2,16 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+'use strict';
+
 
 //alert("生如蝼蚁，应有不屈之心");
 
 
 var problem = {     //观察“一起帮”的求助，定义一个problem对象，其中包含title、body、reward、needRemote等属性
-    title = "string",
-    body = "string",
-    reward = Number,
-    needRemote = Boolean,
+    title : "string",
+    body : "string",
+    reward : Number,
+    needRemote: Boolean,
 }
-
-
 
