d03ae459d3d5031fbd67813fd009a053924d320a
965330241@qq.com <965330241@qq.com>
2021-08-12 18:50:24 +0800

    改造Entity类，让其Id可以为任意类型

--- CSharp/Entity.cs
+++ CSharp/Entity.cs
@@ -5,10 +5,11 @@ using System.Text;
 namespace CSharp
 {
     //再为之前所有类（含User、HelpMoney等）抽象一个基类：Entity，包含一个只读的Id属性。
-    class Entity
+    //改造Entity类，让其Id可以为任意类型
+    class Entity<T>
     {
-        protected int _Id;
-        public int Id
+        protected int _Id ;
+        public int Id 
         {
             get { return _Id; }
 
