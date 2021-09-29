f9ed102967909b6735c90c891f198db7210a86cc
965330241@qq.com <965330241@qq.com>
2021-09-29 14:33:42 +0800

    参数化查询，事务帮帮币

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -68,45 +68,94 @@ namespace Sourcestack
             //        command1.ExecuteNonQuery();
             //    }
             //单页呈现
-            using (DbConnection connection = new SqlConnection(connectionString))
-                //{
-                //	connection.Open();
-                //	DbCommand command = new SqlCommand();
-                //	command.Connection = connection;
-                //	command.CommandText = "SELECT * FROM Problem";
-                //	DbDataReader dbDataReader = command.ExecuteReader();
-                //	while (dbDataReader.Read())
-                //	{
-                //		for (int i = 0; i < dbDataReader.FieldCount; i++)
-                //		{
-                //			Console.Write(dbDataReader[i]+"     ");
-                //		}
-                //		Console.WriteLine();
-                //	}
-                //}
-                //		列表页呈现（包括：过滤 / 分页）
-                Console.WriteLine("选择跳转到第几页");
-            int option = int.Parse(Console.ReadLine());
+            // using (DbConnection connection = new SqlConnection(connectionString))
+            //{
+            //	connection.Open();
+            //	DbCommand command = new SqlCommand();
+            //	command.Connection = connection;
+            //	command.CommandText = "SELECT * FROM Problem";
+            //	DbDataReader dbDataReader = command.ExecuteReader();
+            //	while (dbDataReader.Read())
+            //	{
+            //		for (int i = 0; i < dbDataReader.FieldCount; i++)
+            //		{
+            //			Console.Write(dbDataReader[i]+"     ");
+            //		}
+            //		Console.WriteLine();
+            //	}
+            //}
+            //		列表页呈现（包括：过滤 / 分页）
+            //    Console.WriteLine("选择跳转到第几页");
+            //int option = int.Parse(Console.ReadLine());
+            //using (DbConnection connection = new SqlConnection(connectionString))
+            //{
+            //    connection.Open();
+            //    DbCommand dbCommand = new SqlCommand();
+            //    dbCommand.Connection = connection;
+
+            //    dbCommand.CommandText = $"SELECT *FROM" +
+            //        $"( SELECT *, ROW_NUMBER() OVER(ORDER BY ID ) DID FROM Problem) pp " +
+            //        $"WHERE pp.DID BETWEEN {option * 3 - 2} AND {option * 3} ";
+            //    DbDataReader dbDataReader = dbCommand.ExecuteReader();
+            //    while (dbDataReader.Read())
+            //    {
+            //        for (int i = 0; i < dbDataReader.FieldCount; i++)
+            //        {
+            //            Console.Write(dbDataReader[i] + "     ");
+            //        }
+            //        Console.WriteLine();
+            //    }
+
+            //}
+
+            //用“参数化查询”改写之前拼接的SQL代码
+
+            string id = Console.ReadLine();
+            using (DbConnection dbConnection = new SqlConnection(connectionString))
+            {
+                dbConnection.Open();
+                DbCommand command = new SqlCommand();
+                command.Connection = dbConnection;
+                command.CommandText = $"SELECT * FROM Problem WHERE ID = {id}";
+                //DbParameter pId = new SqlParameter("@id", id);
+                //command.Parameters.Add(pId);
+                DbDataReader dbDataReader = command.ExecuteReader();
+                dbDataReader.Read();
+                Console.WriteLine($"{dbDataReader[0]}+{dbDataReader[1]}");
+            }
+
+            //用事务完成帮帮币的交易
+            int amount = int.Parse(Console.ReadLine());
+            int balance = int.Parse(Console.ReadLine());
+            int userId = int.Parse(Console.ReadLine());
             using (DbConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                DbCommand dbCommand = new SqlCommand();
+                IDbCommand dbCommand = new SqlCommand();
                 dbCommand.Connection = connection;
-
-                dbCommand.CommandText = $"SELECT *FROM" +
-                    $"( SELECT *, ROW_NUMBER() OVER(ORDER BY ID ) DID FROM Problem) pp " +
-                    $"WHERE pp.DID BETWEEN {option * 3 - 2} AND {option * 3} ";
-                DbDataReader dbDataReader = dbCommand.ExecuteReader();
-                while (dbDataReader.Read())
+                using (IDbTransaction transaction = connection.BeginTransaction())
                 {
-                    for (int i = 0; i < dbDataReader.FieldCount; i++)
+                    try
                     {
-                        Console.Write(dbDataReader[i] + "     ");
+                        dbCommand.Transaction = transaction;
+                        dbCommand.CommandText = $"INSERT BangMoney  VALUES(@amount,@balace,@userId)";
+                        DbParameter dbamount = new SqlParameter("@amount", amount);
+                        DbParameter dbbalace = new SqlParameter("@balace", balance);
+                        DbParameter dbuserId = new SqlParameter("@userId", userId);
+
+                        dbCommand.Parameters.Add(dbamount);
+                        dbCommand.Parameters.Add(dbbalace);
+                        dbCommand.Parameters.Add(dbuserId);
+                        dbCommand.ExecuteNonQuery();
+                        
+                        transaction.Commit();
                     }
-                    Console.WriteLine();
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
                 }
-
-
             }
         }
     }
